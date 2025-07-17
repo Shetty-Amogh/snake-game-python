688c9807e5237c68da1e128004d1c234eb2d5ba1
Amogh Shetty <amoghshetty.sj@gmail.com>
2025-07-17 08:53:37 +0530

    second commit

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import os
 map = [
 ["P",".","A"],
 [".",".","."],
-[".",".","."]
+[".",".","L"]
 ]
 mapLen = 3
 gameOn = True
 snakeX = 0
 snakeY = 0
+tempX = 0
+tempY = 0
 arrSnakeX = [0]
 arrSnakeY = [0] 
 #-------------------functions--------------------
@@ -20,11 +22,30 @@ def printMap():
         print()
 
 
-def moveSnake():
+def moveSnakePos():
     global arrSnakeX
     global arrSnakeY
     for i in range(len(arrSnakeX),0,-1):
-        print("to code")
+        if i == 1:
+            arrSnakeX[i-1] = snakeX
+            arrSnakeY[i-1] = snakeY
+        elif i == 2:
+            map[arrSnakeX[i-1]] = map[arrSnakeX[i-2]]
+            map[arrSnakeY[i-1]] = map[arrSnakeY[i-2]]
+            arrSnakeX[i-1] = arrSnakeX[i-2]
+            arrSnakeY[i-1] = arrSnakeY[i-2]
+        else:
+            map[arrSnakeX[i-1]] = map[arrSnakeX[i-2]]
+            map[arrSnakeY[i-1]] = map[arrSnakeY[i-2]]
+            arrSnakeX[i-1] = arrSnakeX[i-2]
+            arrSnakeY[i-1] = arrSnakeY[i-2]
+
+def moveSnake():
+    for i in range(len(arrSnakeX)):
+        if i ==0:
+         continue
+        else:
+         map[arrSnakeX[i+1]] = map[arrSnakeX[i]]
 
 #------------------- main game ------------------
 
@@ -40,23 +61,25 @@ while (gameOn == True):
             snakeX += 1
             arrSnakeX.append(snakeX)
             arrSnakeY.append(snakeY)
-            moveSnake()
+            moveSnakePos()
         else :
             temp =  map[snakeY][snakeX + 1]
             map[snakeY][snakeX + 1] =  map[snakeY][snakeX]
             map[snakeY][snakeX] = temp
             snakeX += 1
+            moveSnakePos()
     elif inputMove == "s" or inputMove == "S":
         if map[snakeY+1][snakeX] == "A":
             map[snakeY+1][snakeX] = map[snakeY][snakeX]
             snakeY += 1
             arrSnakeX.append(snakeX)
             arrSnakeY.append(snakeY)
-            moveSnake()
+            moveSnakePos()
         else :
             temp =  map[snakeY+1][snakeX]
             map[snakeY+1][snakeX] =  map[snakeY][snakeX]
             map[snakeY][snakeX] = temp
             snakeY += 1
+            moveSnakePos()
     elif inputMove == "exit":
         break
